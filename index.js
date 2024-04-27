ff4b809c5288999921fd0f589c55c126bae2656a
R Kanishkasree <kanishkasree804@gmail.com>
2024-04-27 17:00:46 +0530

    basic setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoos = require('mongoose')
 const bodyparser = require('body-parser')
 const cors = require('cors')
-
+const uploadRouter = require('./upload');
 const app = express()
 app.use(bodyparser.json())
 app.use(cors()) 

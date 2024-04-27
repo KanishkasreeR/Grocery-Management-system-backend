5d5b51b06b8d5245c52aae44bcc7614495296a2c
R Kanishkasree <kanishkasree804@gmail.com>
2024-04-27 17:48:02 +0530

    Basic setup

--- addproduct.js
+++ addproduct.js
@@ -1,4 +1,39 @@
-const mongoos = require('mongoose')
+// const mongoos = require('mongoose')
+
+// const ProductSchema = mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true
+//         // unique: true
+//     },
+//     price: {
+//         type: Number,
+//         required: true
+//     },
+//     quantity: {
+//         type: String,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     image: {
+//         type: String,
+//         required: false
+//     },
+//     createdDate: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+
+// const Product = mongoose.model('Product', ProductSchema);
+
+// export default Product;
+
+
+const mongoose = require('mongoose');
 
 const ProductSchema = mongoose.Schema({
     title: {
@@ -30,4 +65,4 @@ const ProductSchema = mongoose.Schema({
 
 const Product = mongoose.model('Product', ProductSchema);
 
-export default Product;
+module.exports = Product;

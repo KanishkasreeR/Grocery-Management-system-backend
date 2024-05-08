9c75da7b6ca038fd0662e0f02c37b091f720460e
R Kanishkasree <kanishkasree804@gmail.com>
2024-05-08 20:18:53 +0530

    Basic setup

--- upload.js
+++ upload.js
@@ -372,6 +372,7 @@ const express = require('express');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const Product = require('./addproduct.js'); // Import your Mongoose Product model
+const wishlists = require('./Wishlist.js');
 
 const router = express.Router();
 

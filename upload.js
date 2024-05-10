49263bab542ae40a98eace6adaee6a3829fdcedc
R Kanishkasree <kanishkasree804@gmail.com>
2024-05-10 19:16:49 +0530

    Basic setup

--- upload.js
+++ upload.js
@@ -615,27 +615,6 @@ router.get('/search', async (req, res) => {
 });
 
 
-
-// router.post('/addToWishlist', (req, res) => {
-//   const { productId, customerId } = req.body;
-  
-//   // Check if customerId exists in the wishlists object, if not, create an entry for the customer
-//   if (!wishlists[customerId]) {
-//     wishlists[customerId] = [];
-//   }
-
-//   // Check if the product is already in the wishlist
-//   if (wishlists[customerId].includes(productId)) {
-//     return res.status(400).json({ error: 'Product already exists in wishlist' });
-//   }
-
-//   // Add the productId to the customer's wishlist
-//   wishlists[customerId].push(productId);
-
-//   // Respond with success message
-//   res.status(200).json({ message: 'Product added to wishlist successfully' });
-// });
-
 router.post('/addToWishlist', async (req, res) => {
   try {
     const { productId, customerId } = req.body;
@@ -675,6 +654,22 @@ router.get('/wishlist', async (req, res) => {
   } catch (error) {
     console.error('Error fetching wishlist products:', error);
     res.status(500).json({ error: 'Failed to fetch wishlist products' });
+  }
+});
+
+router.get('/products/:productId', async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+    
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error('Error fetching product:', error);
+    res.status(500).json({ error: 'Failed to fetch product' });
   }
 });
 

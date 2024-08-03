a69c58cca75f0df2721d2971741bedf15bdf6f29
R Kanishkasree <124056440+KanishkasreeR@users.noreply.github.com>
2024-08-03 12:30:44 +0530

    OrderSchema.js

--- OrderSchema.js
+++ OrderSchema.js
@@ -1,17 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const orderSchema = new mongoose.Schema({
-//   customerId: String,
-//   products: Array,
-//   adminId: String,
-//   totalPrice: Number,
-//   orderDate: { type: Date, default: Date.now },
-// });
-
-// const Order = mongoose.model('Order', orderSchema);
-// module.exports = Order;
-
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
